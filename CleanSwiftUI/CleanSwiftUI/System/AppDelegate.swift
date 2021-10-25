d624d693a32a6ffa5869aa6a5e6c64b199ce06a2
Aike Fernández Roza <aikesiero@gmail.com>
2021-10-25 13:32:27 +0200

    Created Project

---
+++ CleanSwiftUI/CleanSwiftUI/System/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  CleanSwiftUI
+//
+//  Created by Aike Fernández Roza on 25/10/21.
+//
+
+import UIKit
+import Combine
+
+@UIApplicationMain
+
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
---
+++ CleanSwiftUI/CleanSwiftUI/System/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  CleanSwiftUI
+//
+//  Created by Aike Fernández Roza on 25/10/21.
+//
+
+import Foundation
+import Combine
+import UIKit
+import SwiftUI
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
