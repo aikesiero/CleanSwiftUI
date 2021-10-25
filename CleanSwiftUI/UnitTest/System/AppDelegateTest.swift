28ab35baa4589c22f1e47c817a4d2ed36a0b3f9c
Aike Fernández Roza <aikesiero@gmail.com>
2021-10-25 23:00:30 +0200

    Added Codemagic

---
+++ CleanSwiftUI/UnitTest/System/AppDelegateTest.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegateTest.swift
+//  UnitTest
+//
+//  Created by Aike Fernández Roza on 25/10/21.
+//
+
+import XCTest
+@testable import CleanSwiftUI
+
+class AppDelegateTest: XCTestCase {
+
+    var sut: AppDelegate?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = AppDelegate()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testDidFinishLaunching() {
+      //  _ = sut?.application(UIApplication.shared, didFinishLaunchingWithOptions: [:])
+    }
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+
+
+}
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Proyecto de demostración con una app usando SwiftUI con Clean Architecture.
 
-
+- Codemagic
 
 ![platforms](https://img.shields.io/badge/platforms-iPhone%20%7C%20iPad%20%7C%20macOS-lightgrey) [![Build Status](https://travis-ci.com/nalexn/clean-architecture-swiftui.svg?branch=master)](https://travis-ci.com/nalexn/clean-architecture-swiftui) [![codecov](https://codecov.io/gh/nalexn/clean-architecture-swiftui/branch/master/graph/badge.svg)](https://codecov.io/gh/nalexn/clean-architecture-swiftui) [![codebeat badge](https://codebeat.co/badges/db33561b-0b2b-4ee1-a941-a08efbd0ebd7)](https://codebeat.co/projects/github-com-nalexn-clean-architecture-swiftui-master)
+
